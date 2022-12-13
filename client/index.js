ef2e1b7f89b8670ec556dca1f54f92e2e5611f9c
Ming Han <minghan.kong@gmail.com>
2022-12-13 15:32:45 +0800

    Implement proof mechanism

--- client/index.js
+++ client/index.js
@@ -4,11 +4,19 @@ const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
-async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+// This goes on the API Server
+const tree = new MerkleTree(niceList);
+
+async function main() {  
+  // This will be the lucky guy who will win the gift
+  const luckyGuy = "Robin Hessel Jr.";
+  const index = niceList.indexOf(luckyGuy);
+
+  // Proof
+  const proof = tree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    proof, name: luckyGuy
   });
 
   console.log({ gift });
